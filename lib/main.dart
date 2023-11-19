167e1e2d147b37563b9d470cfcb057f310e20427
rokasltu <rjucys@gmail.com>
2023-11-19 19:25:35 +0200

    pakeistas pradinis skaicius

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
+  int _counter = 110;
 
   void _incrementCounter() {
     setState(() {
